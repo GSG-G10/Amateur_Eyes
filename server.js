78316ece2538cb0c5fa7d9d9da302e9218aee5e6
ElhamFadel <elham2000Fadel@gmail.com>
2021-08-11 12:20:24 +0300

    add 404 and 500 html file #15 - initial server relate #11

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const compression = require('compression');
+// disable powered by express header
+app.disable('x-powered-by');
+// set port
+const port = process.env.PORT || 5000;
+app.set('port', port);
+app.use(compression())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+// set static files location
+app.use(express.static(__dirname + '/public'));
+// this will improve when 
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'views','index.html'));
+})
+app.listen(port, () => {
+    console.log('Server listening on port ' + port);
+});
